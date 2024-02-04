835b3e85a57083ca58c5fe5f0bc1bcd5625b96f5
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2024-02-04 02:01:13 +0000

    Configure Sentry

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,8 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
         RAILS_SERVER_URL_DEV: "http://localhost:3000/api/socket-server",
-        RAILS_SERVER_URL_PRODUCTION: "https://gamifycoding.me/api/socket-server",
+        RAILS_SERVER_URL_PRODUCTION:
+          "https://gamifycoding.me/api/socket-server",
       },
     },
   ],
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
-import express from "express";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
+import express, { Request, Response, NextFunction } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import railsController from "./controllers/rails";
+
+interface ResponseWithSentry extends Response {
+  sentry: string;
+}
 
 // const whitelistCors = [
 //   "http://localhost:3000",
@@ -14,6 +20,32 @@ import railsController from "./controllers/rails";
 // ];
 
 const app = express();
+
+process.env.NODE_ENV === "production" &&
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({ app }),
+      new ProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+    enabled: process.env.NODE_ENV === "production",
+  });
+
+// The request handler must be the first middleware on the app
+process.env.NODE_ENV === "production" &&
+  app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+process.env.NODE_ENV === "production" &&
+  app.use(Sentry.Handlers.tracingHandler());
+
 const server = createServer(app);
 app.use(cors());
 
@@ -24,9 +56,35 @@ const io = new Server(server, {
 
 app.use("/api", railsController(io));
 
+// The error handler must be registered before any other error middleware and after all controllers
+process.env.NODE_ENV === "production" &&
+  app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+process.env.NODE_ENV === "production" &&
+  app.use(
+    "*",
+    function onError(
+      err: any,
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ): void {
+      // The error id is attached to `res.sentry` to be returned
+      // and optionally displayed to the user for support.
+      res.statusCode = 500;
+      res.end((res as ResponseWithSentry).sentry + "\n");
+    }
+  );
+
 const PORT =
   (process.env.PORT !== undefined && parseInt(process.env.PORT)) || 4000;
 
 server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+
+process.env.NODE_ENV === "production" &&
+  Sentry.captureMessage(
+    "New Start-Up On Production Server (realtime-backend/info)"
+  );
